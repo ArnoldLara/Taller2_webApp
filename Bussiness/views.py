7a8bc9e030218cbecbf14688f903a3d8d5aa7ed1
ArnoldLara <you@example.com>
2020-04-20 19:39:22 -0500

    Se implementa barra de busqueda por lugar. Tambien se crean aplicaciones para negocios y recomendaciones.

--- Bussiness/views.py
+++ Bussiness/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.views.generic.list import ListView
+
+from .models import Bussiness
+
 # Create your views here.
+
+class BussinessSearchListView(ListView):
+    template_name = 'Bussiness/search.html'
+
+    def get_queryset(self):
+        #print(Bussiness.objects.filter(city__icontains=self.query()))
+        return Bussiness.objects.filter(city__icontains=self.query())
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        print(context)
+        return context
--- yelp_web/urls.py
+++ yelp_web/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('usuarios/login',views.login_view,name='login'),
     path('usuarios/logout',views.logout_view,name='logout'),
     path('usuarios/registro',views.registro,name='registro'),
+    path('bussiness/', include('Bussiness.urls')),
 ]
