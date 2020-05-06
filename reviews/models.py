a27d98ddb8b33da5db8a2bf7ec80154fee4a4f02
ArnoldLara <you@example.com>
2020-05-06 12:18:57 -0500

    Se actualiza la base de reviews y su visualizacion en el template.

--- reviews/models.py
+++ reviews/models.py
@@ -5,9 +5,10 @@ class reviews(models.Model):
     user_id = models.CharField(max_length=50)
     name = models.CharField(max_length=10)
     bussiness_name = models.CharField(max_length=20)
-    city = models.CharField(max_length=20)
+    city = models.CharField(max_length=50)
+    text = models.TextField(max_length=500, default='')
     stars = models.DecimalField(max_digits=8, decimal_places=2, default=0.0)
-    date = models.CharField(max_length=20)
+    date = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
--- script_opiniones.py
+++ script_opiniones.py
@@ -3,13 +3,13 @@ import csv
 
 
 with open('reviews.csv', newline='', encoding = 'unicode_escape') as f:
-    reader = csv.reader(f, delimiter=';')
+    reader = csv.reader(f, delimiter=',')
     opinion = list(reader)
 #print(opinion)
 y=0
 for x in opinion:
     print(y,"\n")
     #print(float(x[4]),"\n")
-    review = reviews(user_id=x[0],name=x[1],bussiness_name=x[2],city=x[3],stars=float(x[4]),date=x[5])
+    review = reviews(user_id=x[0],name=x[1],bussiness_name=x[2],city=x[3],stars=float(x[4]),text=x[5],date=x[6])
     review.save()
     y=y+1
